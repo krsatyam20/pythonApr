bfb4313c5a28890111b12a41ed204cb53d42a349
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-05-30 20:19:29 +0530

    Add files via upload

---
+++ nestedLoop.py
@@ -0,0 +1,12 @@
+for x in range(1,3):
+    if x ==3-1:
+     print("======%d Row of x ===="%(x))
+    else:
+       print("==== %d Row of x===== "%(x)) 
+    print("%d X value"%(x))
+    for y in range(1,3):
+        print("%d y value"%(y))
+        
+    
+            
+    
